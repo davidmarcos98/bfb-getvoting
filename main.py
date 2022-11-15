c2456cdd7b93f99abc09f0a9830b3edf94f16e0d
David Marcos <david@citymapper.com>
2022-11-15 02:56:55 +0100

    updates

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from colorama import Fore
 
 from services.datawrapper_service import add_charts
 from services.google_service import get_sheet_rows
+from services.webflow_service import update_collection
+
 sheet_rows = get_sheet_rows()
 
-charts = add_charts(sheet_rows)
+# charts = add_charts(sheet_rows)
+update_collection(sheet_rows)
 
 print(Fore.CYAN + "\nFinished running the script\n")
